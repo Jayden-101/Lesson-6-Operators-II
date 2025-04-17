e7790cf65015494fae0fc51eaf892ef60b6d728b
Jayden-101 <jaydenmaxmillian2@gmail.com>
2025-04-17 17:59:53 +1200

    Not equal

---
+++ Notequal.py
@@ -0,0 +1,24 @@
+a = 10
+b = 12
+c = 12
+
+print(a !=b)
+print(b !=c)
+
+a2 = "python"
+b2 = "coding"
+
+if a2 != b2 :
+    print(a2, 'and' , b2, 'are different.')
+
+
+c = 4
+d = 5
+
+if (c == 1) != (b == 5):
+    print("Hello")
+
+n = int(input("enter a number"))
+
+if n%2 != 0 :
+    print(n, "is not even number")
