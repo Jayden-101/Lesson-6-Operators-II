7edf294c79be96f5b07406d384c353cf62a67696
Jayden-101 <jaydenmaxmillian2@gmail.com>
2025-04-17 18:15:13 +1200

    Asignment

---
+++ asignment.py
@@ -0,0 +1,6 @@
+char = input("Enter a character: ")
+
+if ('a' <= char <= 'z') or ('A' <= char <= 'Z'):
+    print("The character is a alphabet.")
+else:
+    print("The character is not a alphabet")
